02fadd133eee68e8f097cf79915eaca2b6240a08
Bruno Ricci <br9191fr@gmail.com>
2025-12-04 18:53:19 +0100

    creation start

--- src/main/java/com/cecurity/app1/App1Application.java
+++ src/main/java/com/cecurity/app1/App1Application.java
@@ -1,13 +1,19 @@
 package com.cecurity.app1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.slf4j.*;
 @SpringBootApplication
 public class App1Application {
-
+    private final static Logger log = LoggerFactory.getLogger( App1Application.class );
 	public static void main(String[] args) {
-		SpringApplication.run(App1Application.class, args);
+        SpringApplication app = new SpringApplication( App1Application.class );
+        app.setHeadless( false );
+        app.setBannerMode( Banner.Mode.OFF );
+        app.setLogStartupInfo( false );
+        log.info( "Log with arguments {}, {} and {}", 1, "2", 3.0 );
+        app.run( args);
 	}
 
 }
