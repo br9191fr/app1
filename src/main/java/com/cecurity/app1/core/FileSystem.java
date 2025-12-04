cc3ff38f60a28a92d307af56c86ec49060537398
Bruno Ricci <br9191fr@gmail.com>
2025-12-04 19:31:37 +0100

    creation continue

---
+++ src/main/java/com/cecurity/app1/core/FileSystem.java
@@ -0,0 +1,35 @@
+package com.cecurity.app1.core;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Future;
+@Component
+public class FileSystem {
+    private final Path root =
+            Paths.get( System.getProperty("user.home") ).resolve( "fs" );
+
+    public FileSystem() {
+        try { if ( ! Files.isDirectory(root) ) Files.createDirectory(root); }
+        catch ( IOException e ) { throw new UncheckedIOException( e ); }
+    }
+
+    public long getFreeDiskSpace() {
+        return root.toFile().getFreeSpace();
+    }
+
+    public byte[] load( String filename ) {
+        try { return Files.readAllBytes( root.resolve( filename ) ); }
+        catch ( IOException e ) { throw new UncheckedIOException( e ); }
+    }
+
+    public void store( String filename, byte[] bytes ) {
+        try { Files.write( root.resolve( filename ), bytes ); }
+        catch ( IOException e ) { throw new UncheckedIOException( e ); }
+    }
+
+}
