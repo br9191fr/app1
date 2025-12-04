7eb5263be5ac99319ebb23b27e58f3d5fdd93ed4
Bruno Ricci <br9191fr@gmail.com>
2025-12-04 11:46:10 +0100

    first commit

---
+++ src/test/java/com/cecurity/app1/App1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecurity.app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
