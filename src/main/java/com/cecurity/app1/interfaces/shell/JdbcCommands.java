fdf3ef91ba0f0b6b1674ef2c55e5e6e14f7c19bd
Bruno Ricci <br9191fr@gmail.com>
2025-12-08 14:39:13 +0100

    add logger to JdbcCommands

--- src/main/java/com/cecurity/app1/interfaces/shell/JdbcCommands.java
+++ src/main/java/com/cecurity/app1/interfaces/shell/JdbcCommands.java
@@ -1,6 +1,8 @@
 package com.cecurity.app1.interfaces.shell;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,7 +11,7 @@ import java.util.List;
 @ShellComponent
 public class JdbcCommands {
     private final JdbcTemplate jdbcTemplate;
-
+    private final static Logger log = LoggerFactory.getLogger( JdbcCommands.class );
     public JdbcCommands( JdbcTemplate jdbcTemplate ) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -20,8 +22,8 @@ public class JdbcCommands {
         String sql = "SELECT manelength FROM Profile WHERE nickname = ?";
         List<Integer> lengths = jdbcTemplate.queryForList( sql, Integer.class,
                 nickname );
-
+        log.info("sql = {}",sql);
         return lengths.isEmpty() ? "Unknown profile for nickname " + nickname
-                : lengths.get( 0 ).toString();
+                : "Length is " + lengths.getFirst().toString();
     }
 }
